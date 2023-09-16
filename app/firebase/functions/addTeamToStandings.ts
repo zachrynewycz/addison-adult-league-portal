4366fa142b0427c6033601e09b88649def588400
Zach Hrynewycz <85086293+zachrynewycz@users.noreply.github.com>
2023-09-16 16:26:31 -0500

    addteam to stadnigs function

---
+++ app/firebase/functions/addTeamToStandings.ts
@@ -0,0 +1,28 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config";
+
+interface ITeamStanding {
+    name: string,
+    wins: number,
+    losses: number,
+    ot_losses: number,
+    points: number,
+    division: number
+}
+
+export const addTeamToStandings = async (teamName: string, divisionNumber: number) => {
+    try {
+        const docData: ITeamStanding = {
+            name: teamName,
+            wins: 0,
+            losses: 0,
+            ot_losses: 0,
+            points: 0,
+            division: divisionNumber,
+        };
+        
+        await addDoc(collection(db, "standings"), docData)
+    } catch (error) {
+        console.error('Error adding document:', error);
+    }
+}
