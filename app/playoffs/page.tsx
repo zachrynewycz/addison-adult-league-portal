110f598a27834ffd7af73c404fd7e63a1357386f
zachrynewycz <hrynewyczz@gmail.com>
2023-10-10 17:39:50 -0500

    width fix

--- app/playoffs/page.tsx
+++ app/playoffs/page.tsx
@@ -8,7 +8,7 @@ function PlayoffsPage() {
     const { divisionNumber } = useAppSelector((state) => state.division);
 
     return (
-        <main className="py-10 mx-auto md:max-w-7xl px-10 xl:px-0">
+        <main className="py-10 mx-auto max-w-6xl px-10 md:px-20 xl:px-0">
             <h1 className="font-calibre_semi_bold text-xl mb-5">D{divisionNumber} Playoff Bracket</h1>
 
             <div className="flex justify-between">
